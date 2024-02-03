f5882c4b48b8430c5c6acbd4ac5d217f946c1438
Zelalem Argaw <thelalemarg@gmail.com>
2024-02-03 17:01:33 +0300

    Create Number of Good Pairs.py

---
+++ Number of Good Pairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        dc = Counter(nums)
+        count = 0
+        for i in dc:
+            n = dc[i]
+            if n > 1:
+                count += n * (n - 1)//2
+        return count
