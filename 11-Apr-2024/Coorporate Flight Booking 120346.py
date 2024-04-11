a1fa4f6e44d78dd152f53e08d7d9026e84ec2e92
Zelalem Argaw <thelalemarg@gmail.com>
2024-04-11 10:04:40 +0300

    Created 'Coorporate Flight Booking' by A2SV Hub

---
+++ 11-Apr-2024/Coorporate Flight Booking 120346.py
@@ -0,0 +1,15 @@
+# Problem: Coorporate Flight Booking - https://leetcode.com/problems/corporate-flight-bookings/
+
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        diff = [0]*(n+1)
+        for i,j,seats in bookings:
+            diff[i-1] += seats
+            if j < n:
+                diff[j] -= seats
+        print(diff)
+        ans = [0] *n
+        ans[0] = diff[0]
+        for i in range(1,n):
+            ans[i] = ans[i-1] + diff[i]         
+        return ans
