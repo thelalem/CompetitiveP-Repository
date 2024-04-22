8d09431fbfd4e37708c9f4867b420371d8ddff00
Zelalem Argaw <thelalemarg@gmail.com>
2024-04-22 20:46:04 +0300

    Created 'Valid parenthesis' by A2SV Hub

---
+++ 22-Apr-2024/Valid parenthesis 130287.py
@@ -0,0 +1,16 @@
+# Problem: Valid parenthesis - https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                element = stack.pop() if stack else '$'
+                if mapping[char] != element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
