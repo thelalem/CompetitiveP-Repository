6370bce61b6edf154b970f1c533758fe3d31b059
Zelalem Argaw <thelalemarg@gmail.com>
2024-05-15 20:30:13 +0300

    Created 'Team Composition: Programmers and Mathematicians' by A2SV Hub

---
+++ 15-May-2024/Team Composition: Programmers and Mathematicians 151351.py
@@ -0,0 +1,7 @@
+# Problem: Team Composition: Programmers and Mathematicians - https://codeforces.com/problemset/problem/1611/B
+
+t = int(input())
+for _ in range(t):
+    a,b = map(int,input().split())
+    max_team = min((a+b)//4, min(a,b))
+    print(max_team)
