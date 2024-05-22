e2bdbf91e0f33d08b27dc3fbcb89a349a6b5a68d
Zelalem Argaw <thelalemarg@gmail.com>
2024-05-22 19:42:05 +0300

    Created 'Maximum Difference Between Node and Ancestor' by A2SV Hub

---
+++ 22-May-2024/Maximum Difference Between Node and Ancestor 157406.py
@@ -0,0 +1,22 @@
+# Problem: Maximum Difference Between Node and Ancestor - https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        def deepSearch(node,cur_min,cur_max):
+            if not node:
+                return cur_max - cur_min
+            cur_min = min(cur_min,node.val)
+            cur_max = max(cur_max,node.val)
+
+            left = deepSearch(node.left,cur_min,cur_max)
+            right = deepSearch(node.right,cur_min,cur_max)
+
+            return max(left,right)
+        
+        return deepSearch(root,root.val,root.val)
