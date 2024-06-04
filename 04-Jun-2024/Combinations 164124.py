48840307b5bdad0578683efac88cacf4aff6ad44
Zelalem Argaw <thelalemarg@gmail.com>
2024-06-04 21:39:45 +0300

    Created 'Combinations' by A2SV Hub

---
+++ 04-Jun-2024/Combinations 164124.py
@@ -0,0 +1,17 @@
+# Problem: Combinations - https://leetcode.com/problems/combinations/
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def backtrack(start,comb):
+            if len(comb) == k:
+                res.append(comb.copy())
+                return
+            
+            for i in range(start,n+1):
+                comb.append(i)
+                backtrack(i+1,comb)
+                comb.pop()
+                
+        backtrack(1,[])
+        return res
