c43475f0ecea28b3d23e5c8e4256673847afa9f7
Zelalem Argaw <thelalemarg@gmail.com>
2024-07-15 20:53:04 +0300

    Created 'Heap Sort' by A2SV Hub

---
+++ 15-Jul-2024/Heap Sort 180561.py
@@ -0,0 +1,35 @@
+# Problem: Heap Sort - https://practice.geeksforgeeks.org/problems/heap-sort/1
+
+class Solution:
+    #Heapify function to maintain heap property.
+    def heapify(self,arr, n, i):
+        largest = i
+        left = 2*i+1
+        right = 2*i +2
+        
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+        
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+            
+        if largest != i:
+            arr[i],arr[largest] = arr[largest],arr[i]
+            self.heapify(arr,n,largest)
+            
+        
+    #Function to build a Heap from array.
+    def buildHeap(self,arr,n):
+        start = n//2-1
+        
+        for i in range(start, -1,-1):
+            self.heapify(arr,n,i)
+    
+    #Function to sort an array using Heap Sort.    
+    def HeapSort(self, arr, n):
+        n = len(arr)
+        self.buildHeap(arr,n)
+        
+        for i in range(n-1, 0, -1):
+            arr[i], arr[0] = arr[0], arr[i] 
+            self.heapify(arr, i, 0)
