a2672ac0f57b874afb9677536dde4afde93715e3
Zelalem Argaw <thelalemarg@gmail.com>
2024-07-20 23:06:07 +0300

    Created 'Thearte Square' by A2SV Hub

---
+++ 20-Jul-2024/Thearte Square 182420.py
@@ -0,0 +1,7 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+n, m, a = map(int, input().split())
+length = (n + a - 1) // a
+width = (m + a - 1) // a
+total = length * width
+print(total)
