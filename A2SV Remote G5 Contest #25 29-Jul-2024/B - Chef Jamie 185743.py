f5948c826873a455edf1b2e090e6ff837aa5d10c
Zelalem Argaw <thelalemarg@gmail.com>
2024-07-29 21:29:25 +0300

    Created 'B - Chef Jamie' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/B - Chef Jamie 185743.py
@@ -0,0 +1,11 @@
+# Problem: B - Chef Jamie - https://codeforces.com/gym/537575/problem/B
+
+n = int(input())
+fruits = list(map(int,input().split()))
+sorted_fruits = sorted(fruits)
+res = 0
+
+for i in range(n):
+    if sorted_fruits[i] != fruits[i]:
+        res += 1
+print(res-1) if res > 0 else print(0)
