58b00c6e3cfb0b4dd09121107da6e0e7b93e8ad3
Zelalem Argaw <thelalemarg@gmail.com>
2024-07-29 21:29:45 +0300

    Created 'C - Experiment' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/C - Experiment 185745.py
@@ -0,0 +1,23 @@
+# Problem: C - Experiment - https://codeforces.com/gym/537575/problem/C
+
+n = int(input())
+max_size = int(1e4+10)
+ps = [0] * max_size
+
+experiments = []
+for _ in range(n):
+    si, ti, bi = map(int, input().strip().split())
+    experiments.append((si, ti, bi))
+for experiment in experiments:
+    s, t, b = experiment
+    ps[s] += b
+    ps[t + 1] -= b
+
+result = 0
+current_rooms = 0
+
+for i in range(1, max_size):
+    current_rooms += ps[i]
+    result = max(result, current_rooms)
+
+print( result)
