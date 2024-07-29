d2cb68e7bc77a49c93f26588631d21a27206fd0a
Zelalem Argaw <thelalemarg@gmail.com>
2024-07-29 21:29:18 +0300

    Created 'A - Valid Parentheses' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/A - Valid Parentheses 185742.py
@@ -0,0 +1,13 @@
+# Problem: A - Valid Parentheses - https://codeforces.com/gym/537575/problem/A
+
+s = input()
+stack = []
+res = 0
+for i in s:
+    if i == '(':
+        stack.append(i)
+    else:
+        if stack:
+            stack.pop()
+            res += 2
+print(res)
