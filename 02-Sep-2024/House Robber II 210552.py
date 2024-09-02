cd694a50776ab3db3cfc24bf1c70f678a7f6e189
Zelalem Argaw <thelalemarg@gmail.com>
2024-09-02 23:27:39 +0300

    Created 'House Robber II' by A2SV Hub

---
+++ 02-Sep-2024/House Robber II 210552.py
@@ -0,0 +1,28 @@
+# Problem: House Robber II - https://leetcode.com/problems/house-robber-ii/
+
+class Solution:
+    def rob_linear(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        return dp[-1]
+
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+        return max(self.rob_linear(nums[1:]), self.rob_linear(nums[:-1]))
