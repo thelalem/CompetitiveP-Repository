b6e6dad4ef3cddc6244925e789a4b0df648722a7
Zelalem Argaw <thelalemarg@gmail.com>
2024-09-04 20:20:09 +0300

    Created 'Partition Equal Subset Sum' by A2SV Hub

---
+++ 04-Sep-2024/Partition Equal Subset Sum 211047.py
@@ -0,0 +1,18 @@
+# Problem: Partition Equal Subset Sum - https://leetcode.com/problems/partition-equal-subset-sum/
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total_sum = sum(nums)
+
+        if total_sum % 2 != 0:
+            return False
+        
+        target = total_sum // 2
+        n = len(nums)
+        dp = [False] * (target + 1)
+        dp[0] = True  
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+        
+        return dp[target]
