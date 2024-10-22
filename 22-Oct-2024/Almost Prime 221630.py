c0bac6585c55531cbde9dfb09c423237b5375c1a
Zelalem Argaw <thelalemarg@gmail.com>
2024-10-22 20:13:26 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 22-Oct-2024/Almost Prime 221630.py
@@ -0,0 +1,15 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+n = int(input())
+prime_factors = [0] * (n + 1)
+for i in range(2, n + 1):
+    if prime_factors[i] == 0:  
+        for j in range(i, n + 1, i):
+            prime_factors[j] += 1
+
+count = 0
+for i in range(1, n + 1):
+    if prime_factors[i] == 2:
+        count += 1
+
+print( count)
